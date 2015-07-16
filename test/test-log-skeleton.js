c9fbc91ef13112f5b3f56b47e239e800d8685476
Mats Julian Olsen <mats@plysjbyen.net>
2015-07-16 10:11:38 +0200

    Expose .log property

--- test/test-log-skeleton.js
+++ test/test-log-skeleton.js
@@ -1,12 +1,12 @@
-var shadower = require('../lib/bunyan-shadower');
+var skeleton = require('../lib/log-skeleton');
 var assert = require('assert');
 var should = require('should');
 
 var obj = {
   info: function () {return 1;}
 }
-var log = shadower(obj);
-describe('bunyan-shadower', function () {
+var log = skeleton(obj);
+describe('log-skeleton', function () {
   describe('working on a mock object', function () {
     it('should return 1 when called on obj', function () {
       var val = log.info('help');
@@ -15,8 +15,14 @@ describe('bunyan-shadower', function () {
   });
   describe('working on a undefined property', function () {
     it('should not throw when called', function () {
-      var mock = shadower(undefined);
+      var mock = skeleton(undefined);
       (function () {mock.info('help')}).should.not.throw();
+    });
+  });
+  describe('should set the log property', function () {
+    it('log property should be the same object', function () {
+      var log = skeleton(obj);
+      assert.equal(log.log, obj);
     });
   });
 });
